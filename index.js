ef54299b82e6736663531004378926a037f6fa09
deep2882004 <67994719+deep2882004@users.noreply.github.com>
2024-07-28 19:26:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
-var randomNumber1 = Math.random();
-    randomNumber1 = randomNumber1*6
-randomNumber1 = Math.floor(randomNumber1)+ 1;
 
-var randomImage = "dice" + randomNumber1 + ".png";
-
-var image1 = document.querySelectorAll("img")[0];
-image1.setAttribute("src",randomImage);
+function rolldice(){
 
 
-var randomNumber2 = Math.random();
-var randomNumber2 = randomNumber2*6;
-randomNumber2 = Math.floor(randomNumber2)+1;
+var randomNumber = Math.random()*6;
+var ALLcover = Math.floor(randomNumber+1);
 
-var randomImage1="dice" + randomNumber2 + ".png";
-var image2 = document.querySelectorAll("img")[1];
-image2.setAttribute("src",randomImage1);
 
-if(randomNumber1 > randomNumber2){
+ var randomImage = "dice" + ALLcover +".png";
+ var images = document.querySelectorAll("img")[0];
+ images.setAttribute("src",randomImage);
+
+ var randomNumber1 = Math.random()*6;
+ var ALLcover1 = Math.floor(randomNumber1 + 1);
+
+ var randomImage1 = "dice"+ALLcover1+".png";
+ var image1 = document.querySelectorAll("img")[1];
+ image1.setAttribute("src",randomImage1);
+
+ if (ALLcover > ALLcover1){
     document.querySelector("h1").innerHTML="PLAYER 1 WON 🚩 ";
+ }
+
+  else if (ALLcover < ALLcover1){
+    document.querySelector("h1").innerHTML="PLAYER 2 WON 🚩";
+  }
+  else{
+   document.querySelector("h1").innerHTML="DRAW ";
+  }
+  
 }
 
-else if (randomNumber1 < randomNumber2){
-    document.querySelector("h1").innerHTML="PLAYER 2 WON 🚩 "
-}
-
-else {
-    document.querySelector("h1").innerHTML="Draw 🤡"
-}
+document.getElementById("reloadButton").addEventListener("click",function(){
+  rolldice();
+});
